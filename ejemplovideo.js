f7a5d2a8debd0514399542af8f81b0a2deca857d
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-03-17 17:19:54 -0300

    commit inicial

---
+++ ejemplovideo.js
@@ -0,0 +1,155 @@
+// requiero módulos y establezco conexión con base de datos
+
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const unless = require("express-unless");
+const bcrypt = require("bcrypt");
+const mysql = require("mysql");
+const util = require ("util");
+
+const app = express();
+const port = process.env.PORT ? process.env.PORT : 3000; // estructura ternaria, un IF completo, se asigna a variable un valor u otro según si se cumple, el signo de pregunta consulta si existe, si no existe hace lo otro
+ 
+app.use(express.json());
+
+const conexion = mysql.createConnection({
+    host: "localhost", 
+    user: "root",
+    password: "", 
+    database: "login"
+});
+
+conexion.connect((error)=>{
+    if(error){throw error;} 
+    console.log("Conexión con la base de datos establecida");
+});
+
+const qy = util.promisify(conexion.query).bind(conexion); // permite el uso de async-away en la conexion con mysql
+
+//establezco middleware de autenticación
+
+const auth = (req, res, next) => {
+    try{
+        let token = req.headers["authorization"];
+        if(!token){
+            throw new Error("no estas logueado");
+        }
+
+        token = token.replace("Bearer ", "");
+
+        jwt.verify(token, "Secret", (err, user) => {
+            if (err) {
+                throw new Error("Token invalido")
+            }
+        });
+
+        next();
+    }
+
+    catch(e) {
+        res.status(403).send({message: e.message});
+    }
+
+} // termina middleware de autenticación
+
+auth.unless = unless; // defino método de excepción al middleware de autenticación
+
+// llamo a método de excepción al middleware de autenticación, que se interpone antes de hacer cualquier cosa
+app.use(auth.unless({
+    path: [
+        {url : "/login", methods: ["POST"]}, //es un array porque podria usar varios metodos separadas por comas
+        {url : "/registro", methods: ["POST"]},
+    ]
+})); 
+
+// paso 1 registración
+app.post("/registro", async (req, res)=>{
+    try{
+        if(!req.body.usuario || !req.body.clave || !req.body.email || !req.body.cel) {
+            throw new Error ("No enviaste todos los datos necesarios");
+        }
+
+        //valido que el usuario no esté ya en la base de datos
+        let query = "SELECT * FROM usuarios WHERE email = ?";
+        let respuesta = await qy(query, [req.body.email]);        
+        if (respuesta.length > 0) {
+            throw new Error ("Ya existe un usuario con ese correo electrónico");
+        }
+        query = "SELECT * FROM usuarios WHERE usuario = ?";
+        respuesta = await qy(query, [req.body.usuario]);        
+        if (respuesta.length > 0) {
+            throw new Error ("Ya existe un usuario con ese nombre");
+        }
+        
+        // si esta todo bien, encripto la clave
+        const claveEncriptada = await bcrypt.hash(req.body.clave, 10); // es asincronica asi que hay que agregarle siempre async al POST
+
+        // Guardo el nuevo registro con la clave encriptada y le muestro al usuario los otros datos
+        query = "INSERT INTO usuarios (usuario, clave, email, cel) VALUE (?, ?, ?, ?)";
+        respuesta = await qy(query, [req.body.usuario, claveEncriptada, req.body.email, req.body.cel]);
+        let idAgregado = respuesta.insertId;
+        query = "SELECT usuario, email, cel from usuarios WHERE id = ?";
+        respuesta = await qy(query, [idAgregado]); 
+        res.json(respuesta[0]);
+    }
+    catch(e){
+        res.status(413).send({message: e.message});
+    }
+});
+
+// paso 2 login
+
+app.post("/login", async (req, res)=> {
+    try {
+        if(!req.body.usuario || !req.body.clave) {
+            throw new Error ("No enviaste todos los datos necesarios");
+        }
+
+        //paso 1: busco el usuario en base de datos
+        let query = "SELECT * from usuarios where usuario = ?";
+        let respuesta = await qy(query, [req.body.usuario]);
+        if(respuesta.length == 0){
+            throw new Error("Ha ingresado un usuario que no existe");
+        } 
+
+        // Paso 2: verificar que clave ingresada coincida con la encriptada en base de datos
+
+        const recuperarClaveEncriptada = respuesta[0].clave;
+        if(!bcrypt.compareSync(req.body.clave, recuperarClaveEncriptada)){
+            throw new Error("Ha ingresado incorrectamente la clave");
+        }
+
+        // paso 3: sesion
+        const tokenData = {
+            usuario: respuesta[0].usuario,
+            email: respuesta[0].email,
+            user_id: respuesta[0].id
+        } // equivale a pulsera de reconocimiento en festivales
+
+        const token = jwt.sign(tokenData, "Secret", {
+            expiresIn : 60 * 60 * 24 // expira en 24 hs
+        })
+        res.send({token});
+    }
+
+    catch(e){
+        res.status(413).send({message: e.message});
+    }
+});
+
+// Si se envía un GET a localhost/ con Authorization como Key y el token como Header, retorna que el logueo fue correcto
+
+app.get("/", (req, res)=> {
+    try {
+        res.send("Logueado correctamente")
+    }
+    catch(e){
+        res.status(413).send({message: e.message});
+    }
+});
+
+// Levantar servidor en el puerto indicado
+
+app.listen(port, ()=> {
+console.log("Servidor escuchando en el puerto", port);
+})
---
+++ usuarios.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-03-2021 a las 02:40:43
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(200) NOT NULL,
+  `clave` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `cel` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
