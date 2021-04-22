6c6d9fb6d202465d9007be2cada7e3cb16feb4a2
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-04-22 12:57:22 -0300

    settings

--- conexionPG.js
+++ conexionPG.js
@@ -1,10 +1,15 @@
 //const settings = require("./settingsPGlocal.json");
-const settings = require("./settingsPG.json");
 const { Client } = require("pg");
 
 module.exports = {
     conectar: function (){
-        const client = new Client (settings); 
+        //const client = new Client (settings);
+        const client = new Client({            
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+              rejectUnauthorized: false
+            }
+          }) 
         client.connect();
         console.log("Conexion con base de datos establecida");
         return client;
