084a4c6262d873d6a0472b59670fc67fae1cc1fb
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-04-22 03:52:48 -0300

    probando

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const rutasUsuarios= require('./rutasUsuarios.js');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //establezco middleware de autenticación
 
